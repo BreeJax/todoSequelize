aae76cb88734386ffc8efeef18011d22726b0437
Bree Jackson <bjacksonsisterwolf@gmail.com>
2017-08-21 14:29:43 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const mustacheExpress = require("mustache-express")
+const bodyParser = require("body-parser")
+const jsonfile = require("jsonfile")
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.engine("mustache", mustacheExpress())
+
+app.set("views", "./templates")
+app.set("view engine", "mustache")
+app.use(express.static("public"))
+
+const models = require("./models")
+models.User.findOne().then(function(user) {
+  console.log(user)
+})
+
+app.listen(3000, () => {
+  console.log("I've got the magic in me!")
+})
